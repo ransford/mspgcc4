17fe75edcda5531d143e9ee1bd4aa4caa1df6744
ma-work <ma-work@9c79f009-54bf-40f1-a1dd-edde5f34ab85>
2009-11-10 16:20:11 +0000

    Overhaul do-libc.sh
    - change all `...` to $(...) to allow for proper quote nesting
    - quote all variables (adjusting sed generators) to make sure we don't
      stumble across blanks, except $# (numeric) and $INSTALL_LAUNCHER.
    - streamline option parser a bit
    - make some tests clearer to read (0 prefix -> _ prefix)
    - remove excess || exit 42 - you can use sh -x instead.
    - keep CVS checkout around to reduce network traffic (doubling
      msp430-libc checkout size for the nonce)
    
    git-svn-id: http://mspgcc4.svn.sourceforge.net/svnroot/mspgcc4@35 9c79f009-54bf-40f1-a1dd-edde5f34ab85

--- do-libc.sh
+++ do-libc.sh
@@ -6,64 +6,67 @@
 
 set -eu
 
-INITIAL_DIR=`pwd`
+INITIAL_DIR="$(pwd)"
 BUILD_DIR=build
 FETCH_ONLY=0
 
-if [ 0$1 = 0 ]; then
+if [ $# -lt 1 ]; then
 	echo "Usage:   do-libc.sh <toolchain target dir> [<build dir>] [--fetch-only]"
 	echo "Example: do-libc.sh /opt/msp430-gcc-latest"
 	exit 1
 fi
 
-TARGET_LOCATION=$1
+TARGET_LOCATION="$1"
+shift
 
-if [ 0$2 != 0 ]; then
-	BUILD_DIR=$2
+if [ $# -ge 1 ] ; then
+	BUILD_DIR="$2"
+	shift
 fi
 
-if [ 0$3 = 0--fetch-only ]; then
+if [ $# -ge 1 ] && [ "_$1" = "--fetch-only" ]; then
 	FETCH_ONLY=1
 fi
 
-INSTALL_LAUNCHER=`sh do-detect-sudo.sh $TARGET_LOCATION` || exit 1
+INSTALL_LAUNCHER="$(sh do-detect-sudo.sh "$TARGET_LOCATION")"
 
 mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
 
-export PATH=$PATH:$TARGET_LOCATION/bin
-TARGET_LOCATION_SED=`echo $TARGET_LOCATION | sed -e "s/\//\\\\\\\\\//g"`
+export PATH="$TARGET_LOCATION/bin:$PATH"
+TARGET_LOCATION_SED="$(echo "$TARGET_LOCATION" | sed -e "s,/,\\\\/,g")"
 
 mkdir -p mspgcc
 cd mspgcc
 
-test -e msp430-libc && rm -rf msp430-libc
-
-cvs -z3 -d:pserver:anonymous@mspgcc.cvs.sourceforge.net:/cvsroot/mspgcc co -P msp430-libc || exit 2
+cvs -z3 -d:pserver:anonymous@mspgcc.cvs.sourceforge.net:/cvsroot/mspgcc co -n -A -P -d msp430-libc.pristine msp430-libc
+rm -rf msp430-libc
+mkdir msp430-libc
 cd msp430-libc
-test -e $INITIAL_DIR/msp430-libc.patch && patch -p1 < $INITIAL_DIR/msp430-libc.patch 
-mkdir src/msp1
-mkdir src/msp2
-cd ../..
-cd mspgcc
+( cd msp430-libc.pristine && tar cf - .) | ( tar xpf - )
+if [ -e "$INITIAL_DIR/msp430-libc.patch" ] ; then 
+	patch -p1 -N -i "$INITIAL_DIR/msp430-libc.patch"
+fi
+mkdir -p src/msp1 src/msp2
+cd ../../mspgcc
 cd msp430-libc/src
 sed -e "s/\/usr\/local\/msp430/$TARGET_LOCATION_SED/" Makefile > Makefile.new
 mv Makefile.new Makefile
 
-if [ 0$FETCH_ONLY = 01 ]; then
+if [ "_$FETCH_ONLY" = _1 ]; then
 	echo "msp430 libc downloaded successfully"
 	exit 0
 fi
 
-PATH=$TARGET_LOCATION/bin:$PATH make
-$INSTALL_LAUNCHER make install || exit 13
+make
+$INSTALL_LAUNCHER make install
 
-echo "!<arch>" > 0lib.tmp
-$INSTALL_LAUNCHER cp 0lib.tmp $TARGET_LOCATION/lib/libstdc++.a || exit 1
+echo '!<arch>' > 0lib.tmp
+$INSTALL_LAUNCHER cp 0lib.tmp "$TARGET_LOCATION/lib/libstdc++.a"
 rm 0lib.tmp
-$INSTALL_LAUNCHER cp $TARGET_LOCATION/msp430/include/sys/inttypes.h $TARGET_LOCATION/msp430/include/inttypes.h  || exit 1
+$INSTALL_LAUNCHER cp "$TARGET_LOCATION/msp430/include/sys/inttypes.h" "$TARGET_LOCATION/msp430/include/inttypes.h"
 
-cd $TARGET_LOCATION/msp430/lib/ldscripts
-$INSTALL_LAUNCHER tar xjf $INITIAL_DIR/ports/ldscripts-new.tbz || exit 14
+cd "$TARGET_LOCATION/msp430/lib/ldscripts"
+$INSTALL_LAUNCHER tar xjf "$INITIAL_DIR/ports/ldscripts-new.tbz"
 
-cd $INITIAL_DIR
+cd "$INITIAL_DIR"
