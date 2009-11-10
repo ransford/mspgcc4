a0c82b18317ac363ba2b2713b9fd86119f37a20f
ma-work <ma-work@9c79f009-54bf-40f1-a1dd-edde5f34ab85>
2009-11-10 17:52:08 +0000

    Quote arguments, use set -e, misc. fixes.
    git-svn-id: http://mspgcc4.svn.sourceforge.net/svnroot/mspgcc4@41 9c79f009-54bf-40f1-a1dd-edde5f34ab85

--- do-gdb.sh
+++ do-gdb.sh
@@ -9,82 +9,83 @@
 # 7.0
 # insight 6.8-1
 
+
+set -eu
+
+# source utilities
+. ./buildgcc.subr
+
 PKG_VERSION=7.0
 PKG_NAME=gdb	#can also be insight
 GNU_MIRROR=ftp.uni-kl.de
 BUILD_DIR=build
-INITIAL_DIR=`pwd`
+INITIAL_DIR="$(pwd)"
+FETCH_ONLY=0
 
-if [ 0$1 = 0 ]; then
+if [ $# -lt 1 ]; then
 	echo "Usage:   do-gdb.sh <toolchain target dir> [<gdb_version>] [<GNU mirror site>] [<build dir>] [gdb/insight] [--fetch-only]"
 	echo "Example: do-gdb.sh /opt/msp430-gcc-latest $PKG_VERSION $GNU_MIRROR build"
 	exit 1
 fi
 
-TARGET_LOCATION=$1
+TARGET_LOCATION="$1"
+shift
 
-if [ 0$2 != 0 ]; then
-	PKG_VERSION=$2
-fi
+if [ $# -ge 1 ] && [ -n "$1" ]; then PKG_VERSION="$1" ; shift ; fi
+if [ $# -ge 1 ] && [ -n "$1" ]; then GNU_MIRROR="$1" ; shift ; fi
+if [ $# -ge 1 ] && [ -n "$1" ]; then BUILD_DIR="$1" ; shift ; fi
+if [ $# -ge 1 ] && [ -n "$1" ]; then PKG_NAME="$1" ; shift ; fi
 
-if [ 0$3 != 0 ]; then
-	GNU_MIRROR=$3
-fi
+while [ $# -ge 1 ] ; do
+	case "$1" in
+		--fetch-only)	FETCH_ONLY=1 ;;
+		*)		echo >&2 "Ambiguous options $@." ; exit 1 ;;
+	esac
+	shift
+done
 
-if [ 0$4 != 0 ]; then
-	BUILD_DIR=$4
-fi
-
-if [ 0$5 != 0 ]; then
-	PKG_NAME=$5
-fi
-
-if [ 0$6 = 0--fetch-only ]; then
-	FETCH_ONLY=1
-fi
-
-case $PKG_NAME in
+case "$PKG_NAME" in
 gdb)
-	PKG_SRC_URL=ftp://$GNU_MIRROR/pub/gnu/gdb/gdb-$PKG_VERSION.tar.bz2
+	PKG_SRC_URL="ftp://$GNU_MIRROR/pub/gnu/gdb/gdb-$PKG_VERSION.tar.bz2"
 	;;
 insight)
-	PKG_SRC_URL=ftp://sourceware.org/pub/insight/releases/insight-$PKG_VERSION.tar.bz2
+	PKG_SRC_URL="ftp://sourceware.org/pub/insight/releases/insight-$PKG_VERSION.tar.bz2"
 	;;
 *)
-	echo "Package name shoulw be either gdb or insight".
+	echo "Package name must be either gdb or insight."
 	exit 1
 	;;
 esac
 
+INSTALL_LAUNCHER=$(sh do-detect-sudo.sh "$TARGET_LOCATION") 
 
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR" 
 
-INSTALL_LAUNCHER=`sh do-detect-sudo.sh $TARGET_LOCATION` || exit 1
+export PATH="$PATH:$TARGET_LOCATION/bin"
 
-mkdir -p $BUILD_DIR
-cd $BUILD_DIR || exit 1
-
-export PATH=$PATH:$TARGET_LOCATION/bin
-
-wget -c $PKG_SRC_URL -O $PKG_NAME-$PKG_VERSION.tar.bz2 || exit 1
+wget -c "$PKG_SRC_URL" -O "$PKG_NAME-$PKG_VERSION.tar.bz2" 
 echo "Unpacking $PKG_NAME..."
-tar xjf $PKG_NAME-$PKG_VERSION.tar.bz2 || exit 1
+tar xjf "$PKG_NAME-$PKG_VERSION.tar.bz2" 
 
-cd $PKG_NAME-$PKG_VERSION
-cp -rf $INITIAL_DIR/ports/gdb-6-and-7/* . || exit 1
+cd "$PKG_NAME-$PKG_VERSION"
+cp -rf "$INITIAL_DIR"/ports/gdb-6-and-7/* . 
 
-if [ 0$FETCH_ONLY = 01 ]; then
+if [ $FETCH_ONLY = 1 ]; then
 	echo "$PKG_NAME $PKG_VERSION downloaded successfully"
 	exit 0
 fi
 
-test -e $INITIAL_DIR/$PKG_NAME-$PKG_VERSION.patch && patch -p1 < $INITIAL_DIR/$PKG_NAME-$PKG_VERSION.patch
+if test -e "$INITIAL_DIR/$PKG_NAME-$PKG_VERSION.patch" ; then
+       patch -p1 < "$INITIAL_DIR/$PKG_NAME-$PKG_VERSION.patch"
+fi
 
 cd ..
-mkdir -p $PKG_NAME-$PKG_VERSION-build
-cd $PKG_NAME-$PKG_VERSION-build
+mkdir -p "$PKG_NAME-$PKG_VERSION-build"
+cd "$PKG_NAME-$PKG_VERSION-build"
 
-`pwd`/../$PKG_NAME-$PKG_VERSION/configure --prefix=$TARGET_LOCATION --target=msp430 || exit 1
-make || exit 1
-$INSTALL_LAUNCHER make install || exit 1
+"$(pwd)/../$PKG_NAME-$PKG_VERSION/configure" "--prefix=$TARGET_LOCATION" --target=msp430 
+make -j$(num_cpus)
+"$INSTALL_LAUNCHER make install 
 
-cd $INITIAL_DIR
+cd "$INITIAL_DIR"
