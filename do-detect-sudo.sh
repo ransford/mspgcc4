16ce56958015f30909b6a8ad6bc73a9f2eaf3793
ma-work <ma-work@9c79f009-54bf-40f1-a1dd-edde5f34ab85>
2009-11-10 17:08:15 +0000

    Rewrite script.
    - Move test functionality into a function
    - Use set -eu
    - Replace longish (and partially broken) echo commands by cat with
      HERE-file (cat <<_EOF).
    
    git-svn-id: http://mspgcc4.svn.sourceforge.net/svnroot/mspgcc4@36 9c79f009-54bf-40f1-a1dd-edde5f34ab85

--- do-detect-sudo.sh
+++ do-detect-sudo.sh
@@ -4,37 +4,45 @@
 # Framework 6 Information Society Technologies Project
 #  "Wirelessly Accessible Sensor Populations (WASP)".
 
-TARGET_LOCATION=$1
-
-if [ 0$TARGET_LOCATION = 0 ]; then
-	echo "do-detect-sudo is used internally only. Do not call it manually" 1>&2
-	echo "Usage: do-detect-sudo.sh <directory to check write access>" 1>&2
+TARGET_LOCATION="$1"
+if [ -z "$TARGET_LOCATION" ]; then
+	cat >&2 <<_EOF
+do-detect-sudo is used internally only. Do not call it manually
+Usage: do-detect-sudo.sh <directory to check write access>
+_EOF
 	exit 1
 fi
+
+set -eu
+
+istgtwritable() {
+	(
+		$INSTALL_LAUNCHER mkdir -p "$TARGET_LOCATION"
+		$INSTALL_LAUNCHER touch "$TARGET_LOCATION"/test.dat
+		$INSTALL_LAUNCHER rm -f "$TARGET_LOCATION"/test.dat
+	) 2>/dev/null
+}
 
 INSTALL_LAUNCHER=
 
 NEED_SUDO=0
-if [ ! -e $TARGET_LOCATION ]; then
-	mkdir $TARGET_LOCATION 2> /dev/null || NEED_SUDO=1
-fi
-touch $TARGET_LOCATION/test.dat 2>/dev/null || NEED_SUDO=1
-rm $TARGET_LOCATION/test.dat 2> /dev/null || NEED_SUDO=1
+istgtwritable || NEED_SUDO=1
 
 if [ $NEED_SUDO = 1 ]; then
-	echo "WARNING! Cannot write to $TARGET_LOCATION! 1>&2"
-	echo "Please ensure your account is mentioned in /etc/sudoers and that sudo is installed 1>&2"
-	echo "All binary installation tasks will be invoked using sudo. 1>&2"
-	sudo sleep 0 || exit 1
+	cat >&2 <<_EOF
+Cannot write to $TARGET_LOCATION. Trying to use sudo. Please ensure that
+1. sudo is installed
+2. your account is privileged in /etc/sudoers
+All binary installation tasks will be invoked using sudo.
+_EOF
 	
 	INSTALL_LAUNCHER=sudo
-
-	if [ ! -e $TARGET_LOCATION ]; then
-		$INSTALL_LAUNCHER mkdir $TARGET_LOCATION || exit 1
+	if ! sudo true || ! istgtwritable ; then
+		cat >&2 <<_EOF
+Even using sudo, $TARGET_LOCATION is not writable. Aborting.
+_EOF
+		exit 1
 	fi
-	$INSTALL_LAUNCHER touch $TARGET_LOCATION/test.dat || exit 1
-	$INSTALL_LAUNCHER rm $TARGET_LOCATION/test.dat || exit 1
-
 fi
 
 echo "$INSTALL_LAUNCHER"
