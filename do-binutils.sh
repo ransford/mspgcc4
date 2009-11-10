e95095a2470845a0d8223be77a385c16d78fcb58
ma-work <ma-work@9c79f009-54bf-40f1-a1dd-edde5f34ab85>
2009-11-10 18:09:42 +0000

    Quote arguments, use set -e, revise option parser, misc. fixes.
    git-svn-id: http://mspgcc4.svn.sourceforge.net/svnroot/mspgcc4@44 9c79f009-54bf-40f1-a1dd-edde5f34ab85

--- do-binutils.sh
+++ do-binutils.sh
@@ -8,65 +8,65 @@
 # 2.19
 # 2.19.1
 
+. ./buildgcc.subr
+
 BINUTILS_VERSION=2.19.1
 GNU_MIRROR=ftp.uni-kl.de
 BUILD_DIR=build
-INITIAL_DIR=`pwd`
+INITIAL_DIR="$(pwd)"
+FETCH_ONLY=0
 
-if [ 0$1 = 0 ]; then
+set -eu
+
+if [ $# = 0 ]; then
 	echo "Usage:   do-binutils.sh <toolchain target dir> [<binutils_version>] [<GNU mirror site>] [<build dir>] [--fetch-only]"
 	echo "Example: do-binutils.sh /opt/msp430-gcc-latest $BINUTILS_VERSION $GNU_MIRROR build"
 	exit 1
 fi
 
-TARGET_LOCATION=$1
+TARGET_LOCATION="$1" ; shift
+if [ $# -ge 1 ] ; then BINUTILS_VERSION="$1" ; shift ; fi
+if [ $# -ge 1 ] ; then GNU_MIRROR="$1" ; shift ; fi
+if [ $# -ge 1 ] ; then BUILD_DIR="$1" ; shift ; fi
 
-if [ 0$2 != 0 ]; then
-	BINUTILS_VERSION=$2
-fi
+while [ $# -ge 1 ] ; do
+	case "$1" in
+		--fetch-only)	FETCH_ONLY=1 ;;
+		*)	echo "Unknown options $@. Abort." ; exit 1;
+	esac
+	shift
+done
 
-if [ 0$3 != 0 ]; then
-	GNU_MIRROR=$3
-fi
+INSTALL_LAUNCHER=$(sh do-detect-sudo.sh $TARGET_LOCATION)
 
-if [ 0$4 != 0 ]; then
-	BUILD_DIR=$4
-fi
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
 
-if [ 0$5 = 0--fetch-only ]; then
-	FETCH_ONLY=1
-fi
+export "PATH=$PATH:$TARGET_LOCATION/bin"
 
-INSTALL_LAUNCHER=`sh do-detect-sudo.sh $TARGET_LOCATION` || exit 1
+wget -c "ftp://$GNU_MIRROR/pub/gnu/binutils/binutils-$BINUTILS_VERSION.tar.bz2"
 
-mkdir -p $BUILD_DIR
-cd $BUILD_DIR || exit 1
-
-export PATH=$PATH:$TARGET_LOCATION/bin
-
-wget -c ftp://$GNU_MIRROR/pub/gnu/binutils/binutils-$BINUTILS_VERSION.tar.bz2 || exit 1
-
-if [ 0$FETCH_ONLY = 01 ]; then
+if [ $FETCH_ONLY = 1 ]; then
 	echo "Binutils $BINUTILS_VERSION downloaded successfully"
 	exit 0
 fi
 
 echo "Unpacking binutils..."
-tar xjf binutils-$BINUTILS_VERSION.tar.bz2
+tar xjf "binutils-$BINUTILS_VERSION.tar.bz2"
 
-cd binutils-$BINUTILS_VERSION
+cd "binutils-$BINUTILS_VERSION"
 
-if [ -e $INITIAL_DIR/binutils-$BINUTILS_VERSION.patch ]
+if [ -e "$INITIAL_DIR/binutils-$BINUTILS_VERSION.patch" ]
 then
-	patch -p1 < $INITIAL_DIR/binutils-$BINUTILS_VERSION.patch
+	patch -p1 < "$INITIAL_DIR/binutils-$BINUTILS_VERSION.patch"
 fi
 
 cd ..
-mkdir -p binutils-$BINUTILS_VERSION-build
-cd binutils-$BINUTILS_VERSION-build
+mkdir -p "binutils-$BINUTILS_VERSION-build"
+cd "binutils-$BINUTILS_VERSION-build"
 
-`pwd`/../binutils-$BINUTILS_VERSION/configure --prefix=$TARGET_LOCATION --target=msp430 || exit 1
-make || exit 1
-$INSTALL_LAUNCHER make install || exit 1
+"$(pwd)/../binutils-$BINUTILS_VERSION/configure" "--prefix=$TARGET_LOCATION" --target=msp430
+make
+$INSTALL_LAUNCHER make install
 
-cd $INITIAL_DIR
+cd "$INITIAL_DIR"
