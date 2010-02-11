70ddff089a0028461c913e1277e05605ad77d2e2
ma-work <ma-work@9c79f009-54bf-40f1-a1dd-edde5f34ab85>
2010-02-11 20:19:37 +0000

    Minor fixes and improvements:
    - check for write errors and abort if any
    - add missing EOL sequence at file end
    - run with -w and use strict
    - fix a perl -w warning and use $lastFunctionName
    - change shebang to let /usr/bin/env search for perl
    - Usage hints to - arguments for stdin/stdout
    
    git-svn-id: http://mspgcc4.svn.sourceforge.net/svnroot/mspgcc4@97 9c79f009-54bf-40f1-a1dd-edde5f34ab85

--- stripdump.pl
+++ stripdump.pl
@@ -1,27 +1,35 @@
-#!/usr/bin/perl
+#! /usr/bin/env perl -w
 
 #This script helps testing compiler changes. It takes a disassembly dump
 #and removes all absolute addresses in all forms from it.
 #The stripped disassembly from old and new compiler versions can then be
 #compared using a GUI tool (e.g. kdiff3) to see what exactly was changed.
 
-if ($#ARGV < 1)
-{
-	print "Usage: stripdump.pl <dump file> <output file>\n";
-	exit;
+use Carp;
+use strict;
+
+my ($lastFunctionName, $lastFunctionAddr, $out);
+
+sub werr() {
+	croak "Write error."
 }
 
-open S, $ARGV[0] || die "Failed to open $ARGV[0]!\n";
-open D, ">$ARGV[1]" || die "Failed to open $ARGV[1]!\n";
+if ($#ARGV < 1)
+{
+	die "Usage: stripdump.pl <dump file> <output file>\nAny of the files can be - to mean stdin or stdout, respectively.\n";
+}
+
+open S, $ARGV[0] || die "Failed to open $ARGV[0] for reading: $!\n";
+open D, ">$ARGV[1]" || die "Failed to open $ARGV[1] for writing: $!\n";
 
 foreach (<S>)
 {
 	chomp;
 	if (/([0-9a-fA-F]{8}) <(.*)>:/)
 	{
-		$lastFunctionName = $2;
 		$lastFunctionAddr = hex($1);
-		print D "$2:\n";
+		$lastFunctionName = $2;
+		print D "$lastFunctionName:\n";
 	}
 	elsif (/^[ ]{4}([0-9a-fA-F]{4}):\t[^\t]+\t(.*)$/)
 	{
@@ -33,16 +41,16 @@ foreach (<S>)
 		$out =~ s/0x[0-9a-fA-F]+(\(r[0-9]{2}\))/off$1/g;
 		$out =~ s/[0-9]+(\(r[0-9]{2}\))/off$1/g;
 		$out =~ s/;abs 0x[0-9a-fA-F]+//g;
-		print D $out;
+		print D $out or werr();
 	}
 	elsif (/^[ ]{4}([0-9a-fA-F]{4}):\t[^\t]+$/)
 	{
 		$out = sprintf("+%04x:\n", hex($1) - $lastFunctionAddr);
-		print D $out;
+		print D $out or werr();
 	}
 	else
 	{
-		print D "$_\n";
+		print D "$_\n" or werr();
 	}
 }
 
