3a7d3d555af966a3f5a88a6002470d66e20962eb
Peter A. Bigot <bigotp@acm.org>
2010-07-16 18:11:23 -0500

    Avoid dereferencing SET_SRC if instruction is not SET.

--- ports/gcc-4.x/gcc/config/msp430/msp430.c
+++ ports/gcc-4.x/gcc/config/msp430/msp430.c
@@ -4924,7 +4924,10 @@ rtx insn;
 		rtx pat = PATTERN (next);
 		rtx src, t;
 
-		if (GET_CODE (pat) == RETURN)
+		if (pat && GET_CODE (pat) == PARALLEL)
+			pat = XVECEXP (pat, 0, 0);
+
+		if (!pat || GET_CODE (pat) != SET)
 			return UNKNOWN;
 
 		src = SET_SRC (pat);
