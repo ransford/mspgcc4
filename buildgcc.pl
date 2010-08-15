21ec13d4f5cb7f57850a35507337454153b379c5
Peter A. Bigot <bigotp@acm.org>
2010-08-15 12:41:55 -0500

    Update versions

--- buildgcc.pl
+++ buildgcc.pl
@@ -144,7 +144,7 @@ $MPFR_VERSION="2.4.1";
 				  {'ver' => '4.4.3', 'config' => '4.x'}, 
 				  );
 				  
-@LIBC_VERSIONS = ('20100815', '20100726', 'ti_20100724');
+@LIBC_VERSIONS = ('20100815', 'ti_20100815', '20100726', 'ti_20100724');
 @GDB_VERSIONS = grep(/^gdb-(.*)\.patch/, split("\n", `ls -1 -r`));
 s/gdb-(.*)\.patch/$1/ foreach(@GDB_VERSIONS);
 @GDB_VERSIONS = sort{GNUVersionToInt($b) <=> GNUVersionToInt($a)}(@GDB_VERSIONS);
