c0e2dda6b6eac5aa7f92623abd0565ae2cefe8b7
Peter A. Bigot <bigotp@acm.org>
2010-11-14 10:11:57 -0600

    SF 3109143: Support value-line MSP430G22x1 devices
    Note that the value-line devices are not compatible with the mainline
    devices with the same generation/series/family numbers: for example, the
    MSP430F2001 has more memory than the MSP430G2001.  Fortunately, the
    MSP430G22x1 devices do not have companion MSP430F22x1 device series, so they
    can be added without conflict (for now).

--- ports/gcc-4.x/gcc/config/msp430/msp430.c
+++ ports/gcc-4.x/gcc/config/msp430/msp430.c
@@ -190,6 +190,11 @@ static struct mcu_type_s msp430_mcu_types[] = {
 	{"msp430x2122",  MSP430_ISA_22, 0},
 	{"msp430x2132",  MSP430_ISA_22, 0},
 
+	{"msp430x2201",  MSP430_ISA_22, 0}, /* Value-Line */
+	{"msp430x2211",  MSP430_ISA_22, 0}, /* Value-Line */
+	{"msp430x2221",  MSP430_ISA_22, 0}, /* Value-Line */
+	{"msp430x2231",  MSP430_ISA_22, 0}, /* Value-Line */
+
 	{"msp430x2232",  MSP430_ISA_22, 0},
 	{"msp430x2252",  MSP430_ISA_22, 0},
 	{"msp430x2272",  MSP430_ISA_22, 0},
